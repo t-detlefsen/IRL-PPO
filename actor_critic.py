working through bug in the loss calculation

actor_critic.py
@@ -11,19 +11,21 @@ class ActorCritic(nn.Module):
                 #Actor architecture
                 n_hl_actor: int =3,# num of hidden layers
                 hl_size_actor =256, # hidden layer sizes
-                activation_actor: str = 'tanh',
-                output_activation_actor: str = 'identity',
+                activation_actor = 'tanh',
+                output_activation_actor = 'identity',
                 # Critic architecture
                 n_hl_critic: int =3,# num of hidden layers
                 hl_size_critic =256, # hidden layer sizes
-                activation_critic: str = 'tanh',
-                output_activation_critic: str = 'identity',
+                activation_critic= 'tanh',
+                output_activation_critic = 'identity',
                 ):
 
         # TODO: Initialize Params
         # TODO: Initialize Actor
         # TODO: Initialize Critic
-        self.logstd=logstd
+        super().__init__()
+        self.logstd=nn.Parameter(torch.ones(1, act_dim))*logstd
+        self.logstd.to(get_device())
         self.actor = build_mlp(
                                 input_size=obs_dim,
                                 output_size=act_dim,
@@ -45,13 +47,19 @@ class ActorCritic(nn.Module):
         self.critic.to(get_device())
 
     def forward(self, observation: torch.FloatTensor):
+        #print(observation.shape)
         action_means= self.actor(observation) # the mean action from each observation in the batch
-        covariance_matrix = torch.diag(torch.exp(self.logstd)) #shape = [action_dim, action_dim]
+        std = torch.exp(self.logstd).to(action_means.device)
+        std = std.expand_as(action_means)
         batch_size=action_means.shape[0]
-        batch_covariance_matrix= covariance_matrix.repeat(batch_size,1,1)#repeats the matrix so each batch has its own covariance matrix
-        
-        action_dist_out = distributions.MultivariateNormal(action_means,
-                                                           scale_tril = batch_covariance_matrix)
+        #print(action_means.shape)
+        covariance_matrix = torch.diag_embed(std)
+
+        batch_covariance_matrix= covariance_matrix.expand(batch_size, -1, -1)
+#repeats the matrix so each batch has its own covariance matrix
+        #print(batch_covariance_matrix.shape)
+        action_dist_out = distributions.MultivariateNormal(action_means.to(get_device()),
+                                                           scale_tril = batch_covariance_matrix.to(get_device()))
         return action_dist_out
 
 

ppo_agent.py
@@ -25,7 +25,7 @@ class PPO:
         self.clip_epsilon=args.clip_epsilon
         self.MSE_loss = nn.MSELoss()
         
-        self.K_epochs=self.K_epochs
+        self.K_epochs=args.K_epochs
        
         self.gae_lambda=args.gae_lambda
         
@@ -33,7 +33,7 @@ class PPO:
         self.act_dim=act_dim
         self.lr_actor=args.learning_rate_actor
         self.lr_critic=args.learning_rate_critic
-        self.train_batch_size=args.mini_batch_size
+        self.train_batch_size=args.minibatch_size
         self.reward_to_go=args.reward_to_go
 
         
@@ -71,8 +71,9 @@ class PPO:
         '''
         # TODO: Query Old Policy
         # TODO: Return action
+        #print(state)
         with torch.no_grad():
-            state = torch.FloatTensor(state).to(get_device())
+            state = state.float().to(get_device())
             action, action_logprob, value = self.old_policy.act(state)
             action =to_numpy(action)
         return action
@@ -87,11 +88,21 @@ class PPO:
 
         # Estimate the advantage ,
         # by querying the neural network that you're using to learn the value function
-
-        values_normalized = self.policy.critic(obs)
+        obs= from_numpy(obs)
+        values_normalized = self.policy.critic(obs).squeeze(-1)
+        values_normalized = values_normalized.reshape(-1) 
+        print("q_val shape= ", q_values.shape)
+        print("qval_ndim= ",q_values.ndim)
+        print("q_val type= ", type(q_values))
+        print("values_normalized_shape= ", values_normalized.shape)
+        print("values_normalized_ndim= ", values_normalized.ndim)
+        print("values_normalized type= ", type(values_normalized))
         assert values_normalized.ndim == q_values.ndim
 
-        values= unnormalize(values_normalized, q_values.mean(), q_values.std())
+        
+        values = unnormalize(values_normalized, q_values.mean(), q_values.std()).detach().cpu().numpy()
+
+        
         
         
         if self.gae_lambda is not None:
@@ -99,19 +110,19 @@ class PPO:
             values = np.append(values, [0])
 
             ## combine rews_list into a single array
-            rewards = np.concatenate(rewards_list)
-
+            rewards_flat = np.concatenate(rewards_list).reshape(-1)
+            print(rewards_flat.shape)
             ## create empty numpy array to populate with GAE advantage
             ## estimates, with dummy T+1 value for simpler recursive calculation
             batch_size = obs.shape[0] #Equal to T
-            advantages = np.zeros(batch_size + 1)
+            advantages = np.zeros_like(q_values)
 
             for i in reversed(range(batch_size)):
 
                 non_terminal_flag=1-terminals[i]#0 when its the last in its trajectory
-                TD_error= rewards[i] + self.gamma *non_terminal_flag* values[i+1] - values[i] #temporal difference error / delta in the hw pdf
+                TD_error= rewards_flat[i] + self.gamma *non_terminal_flag* values[i+1] - values[i] #temporal difference error / delta in the hw pdf
                 advantages[i] = TD_error + self.gamma * self.gae_lambda * non_terminal_flag * advantages[i+1] #recursive calculation of the advantage form pdf. non_terminal_flag ensures advantages arent used at ends of trajectories
-            advantages = advantages[:-1]
+            #advantages = advantages[:-1]
 
         else:
         
@@ -134,6 +145,7 @@ class PPO:
         all_logs = []
        # Collects batch of data from replay buffer
         ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.sample_replay_buffer(self.train_batch_size)
+       
         q_vals= calculate_q_vals(rewards_list=re_batch,
                                     gamma=self.gamma,
                                     rtg=self.reward_to_go)
@@ -141,7 +153,23 @@ class PPO:
                                                 rewards_list=re_batch,
                                                 q_values=q_vals,
                                                 terminals=terminal_batch)
-        ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.sample_replay_buffer(self.train_batch_size)
+        T, N, obs_dim  = ob_batch.shape      # (50, 112, obs_dim)  for example
+        _, _, act_dim  = ac_batch.shape
+        ob_batch       = ob_batch.reshape(-1, obs_dim)
+        ac_batch       = ac_batch.reshape(-1, act_dim)
+        advantages     = advantages.reshape(-1)
+        q_vals         = q_vals.reshape(-1)
+        terminal_batch = terminal_batch.reshape(-1)
+
+
+        ob_batch = from_numpy(ob_batch)
+        ac_batch = from_numpy(ac_batch)
+        
+        re_batch = np.array(re_batch)
+        re_batch=from_numpy(re_batch)
+        print("re_batch shape = ", re_batch.shape)
+        advantages = from_numpy(advantages)
+        
         all_logs = []
         for train_step in range(self.K_epochs):
             logprobs, state_values, dist_entropy = self.policy.evaluate(observation=ob_batch, action=ac_batch)
@@ -152,7 +180,7 @@ class PPO:
 
             # Finding Surrogate Loss  
             surr1 = ratios * advantages
-            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
+            surr2 = torch.clamp(ratios, 1-self.clip_epsilon, 1+self.clip_epsilon) * advantages
 
             # final loss of clipped objective PPO
             loss = -torch.min(surr1, surr2) + 0.5 * self.MSE_loss(state_values, re_batch) - 0.01 * dist_entropy
@@ -161,7 +189,7 @@ class PPO:
             self.optimizer.zero_grad()
             loss.mean().backward()
             self.optimizer.step()
-        self.policy_old.load_state_dict(self.policy.state_dict())
+        self.old_policy.load_state_dict(self.policy.state_dict())
         self.replay_buffer.clear()
         return {
             'Training Loss': to_numpy(loss),

utils.py
@@ -23,8 +23,8 @@ def build_mlp(
         output_size: int,
         n_layers: int,
         size: int,
-        activation: Activation = 'tanh',
-        output_activation: Activation = 'identity',
+        activation = 'tanh',
+        output_activation = 'identity',
 ):
     _str_to_activation = {
     'relu': nn.ReLU(),
@@ -48,10 +48,9 @@ def build_mlp(
         returns:
             output_placeholder: the result of a forward pass through the hidden layers + the output layer
     """
-    if isinstance(activation, str):
-        activation = _str_to_activation[activation]
-    if isinstance(output_activation, str):
-        output_activation = _str_to_activation[output_activation]
+    
+    activation = _str_to_activation[activation]
+    output_activation = _str_to_activation[output_activation]
     layers = []
     in_size = input_size
     for _ in range(n_layers):
@@ -62,7 +61,7 @@ def build_mlp(
     layers.append(output_activation)
     return nn.Sequential(*layers)
 
-device = torch.device("gpu")
+device = torch.device("cuda")
 def init_gpu(use_gpu=True, gpu_id=0):
     global device
     if torch.cuda.is_available() and use_gpu:
@@ -105,7 +104,9 @@ def _discounted_cumsum(rewards,gamma):
         diff_matrix= time_indxs-time_col_vect #matrix of differences shape (traj_length, traj_length)
         disc_fact_matrix= np.power(gamma, diff_matrix) #matrix of discount factors shape (traj_length, traj_length)
         disc_fact_matrix=np.triu(disc_fact_matrix)#need the upper triangular matrix vals
-        rtgs=disc_fact_matrix @ rewards.reshape(-1,1)
+        # print("disc_factor_matrix= ",disc_fact_matrix.shape)
+        # print("rwds shape= ",rewards.shape)
+        rtgs=disc_fact_matrix @ rewards
         return rtgs.flatten() 
 
 def _discounted_return(rewards,gamma):
@@ -152,8 +153,8 @@ def calculate_q_vals( rewards_list,gamma,rtg:bool=True):
     return q_vals  # return an array
 
 
-def sample_trajectory(env, policy, num_steps_per_rollout, render=False, render_mode=('rgb_array')):
-    ob = env.reset()
+def sample_trajectory(env, policy, num_steps_per_rollout, seed:int):
+    ob, _ = env.reset(seed=seed)
     obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
     steps = 0
     while True:
@@ -170,11 +171,14 @@ def sample_trajectory(env, policy, num_steps_per_rollout, render=False, render_m
         #         env.render(mode=render_mode)
         #         time.sleep(env.model.opt.timestep)
         obs.append(ob)
-        ac = policy.select_action(ob)[0]# HINT: query the policy's get_action function [OK]
-        acs.append(ac)
+        ac_multi = policy.select_action(ob)# HINT: query the policy's get_action function [OK]
+        #print ("multi action size= ",ac_multi.shape)
+       
+        
+        acs.append(ac_multi)
 
         # take that action and record results
-        ob, rew, done, _ = env.step(ac)
+        ob, rew, done,_,info = env.step(ac_multi)
 
         # record result of taking that action
         steps += 1
@@ -182,8 +186,8 @@ def sample_trajectory(env, policy, num_steps_per_rollout, render=False, render_m
         rewards.append(rew)
 
        
-        # HINT: rollout can end due to done, or due to max_path_length
-        if (done==True) or (steps>=num_steps_per_rollout):
+        # HINT: rollout can end due to max_path_length
+        if (steps>=num_steps_per_rollout):
             rollout_done = 1 
         else:
             rollout_done = 0
@@ -193,13 +197,13 @@ def sample_trajectory(env, policy, num_steps_per_rollout, render=False, render_m
             break
     return Path(obs, image_obs, acs, rewards, next_obs, terminals)
 
-def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
+def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, seed:int):
     #  get this from hw1
 
     timesteps_this_batch = 0
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
-        path=sample_trajectory(env, policy, max_path_length, render, render_mode)
+        path=sample_trajectory(env, policy, max_path_length, seed)
         paths.append(path)
         timesteps_this_batch = timesteps_this_batch+get_pathlength(path)
         print('At timestep:    ', timesteps_this_batch, '/', min_timesteps_per_batch, end='\r')
@@ -225,11 +229,12 @@ def Path(obs, image_obs, acs, rewards, next_obs, terminals):
     """
     if image_obs != []:
         image_obs = np.stack(image_obs, axis=0)
-    return {"observation" : np.array(obs, dtype=np.float32),
+    
+    return {"observation" : np.array([o.cpu().numpy() for o in obs], dtype=np.float32),
             "image_obs" : np.array(image_obs, dtype=np.uint8),
-            "reward" : np.array(rewards, dtype=np.float32),
+            "reward" : np.array([r.cpu().numpy() for r in rewards], dtype=np.float32),
             "action" : np.array(acs, dtype=np.float32),
-            "next_observation": np.array(next_obs, dtype=np.float32),
+            "next_observation": np.array([no.cpu().numpy() for no in next_obs], dtype=np.float32),
             "terminal": np.array(terminals, dtype=np.float32)}
 
 